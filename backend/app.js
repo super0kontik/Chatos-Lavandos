09a1fb190e03196d78074cc66db7f7dc6b82ea45
super0kontik <pav10kont@gmail.com>
2019-10-09 18:05:59 +0300

    passport init

--- backend/app.js
+++ backend/app.js
@@ -3,13 +3,30 @@ const bp = require('body-parser');
 const socket = require('socket.io');
 const cors = require('cors');
 const app = express();
+const passport = require('passport');
+require('./passport/google-strat');
 
 app.use(bp.json());
 app.use(bp.urlencoded({extended:false}));
 app.use(cors());
+app.use(passport.initialize());
 
 app.get('/', (req,res)=>{
     res.send('hello')
 });
+
+app.get(
+    '/auth',
+    passport.authenticate('google', {
+        scope: ['profile']
+    })
+);
+
+app.get(
+    '/auth/callback',
+    passport.authenticate('google', { failureRedirect: '/' }),(req, res) => {
+        res.send(req.user)
+    }
+);
 
 module.exports= app;
--- backend/config/config.js
+++ backend/config/config.js
@@ -1,4 +1,8 @@
 module.exports = {
     PORT:8080,
-    MONGO_URL:'mongodb://localhost:27017/chat'
+    MONGO_URL:'mongodb://localhost:27017/chat',
+    callbackURL:"http://localhost:8080/auth/callback",
+    clientID:'138347420904-b72oif3jnot616rmd8higgulsol512t7.apps.googleusercontent.com',
+    clientSecret:'x352IT8kwFnYGUEPJapldCuY',
+    SECRET_WORD:'keyboard'
 };
