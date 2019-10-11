6a30de8e5aa6a35de00e5e3e62e4f721c5320592
super0kontik <pav10kont@gmail.com>
2019-10-11 15:50:21 +0300

    add message listener

--- backend/socket.js
+++ backend/socket.js
@@ -1,12 +1,28 @@
-const socketIO = require('socket.io');
-module.exports= (server) => {
+import {messages} from "../client/src/app/main/room/room.component";
 
+const socketIO = require('socket.io');
+const User = require('./models/user');
+const Room = require('./models/room');
+const Message = require('./models/message');
+const jwt = require('jsonwebtoken');
+
+module.exports = (server) => {
     const io = socketIO(server);
+
     io.on('connection', socket => {
+        console.log('connected');
         socket.join('common');
-        if (socket.handshake.headers.Authorization === 'Bearer js') {
-            io.to('common').emit('join', {message: "welcome, js fan"})
-        }
+            if (socket.handshake.headers.Authorization === 'Bearer js') {
+                console.log('passed');
+                io.to('common').emit('join', {message: "welcome, js fan"})
+            }else{
+                console.log('forbidden')
+            }
+
+            io.on('createMessage', params=>{
+                io.to(params.room).emit('newMessage', {message:params.message})
+        })
+
     });
 
 };
