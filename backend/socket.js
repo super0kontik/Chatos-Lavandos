e21be1266ea77f4788106fe30f8d49f5b68be688
super0kontik <pav10kont@gmail.com>
2019-10-15 13:01:05 +0300

    test auth

--- backend/socket.js
+++ backend/socket.js
@@ -8,21 +8,20 @@ const {SECRET_WORD} = require('./config/config');
 module.exports = (server) => {
     const io = socketIO(server);
 
-    io.on('connection', socket => {
-        const sockets = Object.keys(io.sockets.sockets);
-        console.log(sockets);
-        socket.join('common');
-        // io.use(sjwt.authorize({
-        //     secret: SECRET_WORD,
-        //     handshake: true
-        // }));
-        io.to(socket.id).emit('join', {message: `welcome, world `});
+    io.sockets.on('connection',sjwt.authorize({
+        secret: SECRET_WORD,
+        }),socket => {
+            const sockets = Object.keys(io.sockets.sockets);
+            console.log(sockets);
+            socket.join('common');
+            io.to(socket.id).emit('join', {message: `welcome, ${socket.decoded_token.name} `})})
 
-        socket.on('createMessage', params=>{
-            console.log(params)
+        .on('createMessage', params=>{
+            console.log(params);
             io.to('common').emit('newMessage', {message:params.message,room:'common'})
         })
 
-    });
+
+
 
 };
