578da0ef4d1ae894b5a9baf183d96e9338599a2e
Pavel Kontugansky <pavel.kontugansky@gmail.com>
2019-12-06 17:13:46 +0200

    add sending all unread messages at first message request

--- backend/controllers/http/messages.js
+++ backend/controllers/http/messages.js
@@ -5,13 +5,21 @@ const Room = require('../../models/room');
 
 module.exports.roomContent = async (req,res)=>{
     try {
-        const limit = +req.query.limit || 50;
+        let limit = +req.query.limit || 50;
         if(req.params.id.trim().length <2 || req.query.offset === undefined || +req.query.offset < 0 || limit === undefined || limit < 0){
             throw new Error('invalid data')
         }
         const room = await Room.findOne({_id:req.params.id, users:req.decoded.id});
         if(!room){
             throw new Error('Not Allowed')
+        }
+        while (+req.query.offset === 0) {
+            const message = await Message.find({room: req.params.id}).skip(limit).limit(1).sort('-createdAt');
+            if (message[0] && !message[0].read.find(i => String(i) === req.decoded.id)) {
+                limit += 50;
+            }else {
+                break;
+            }
         }
         const messages = await Message.find({room: req.params.id}).skip(+req.query.offset).limit(limit).sort('-createdAt').populate('creator');
         if(messages) {
